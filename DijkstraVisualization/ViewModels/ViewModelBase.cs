6f7f51ddce859241ed32cb46f98b66c0aa4641f5
Jan3kk <jasiu4d@wp.pl>
2025-12-20 10:30:42 +0100

    init: re-initialize working directory to use Avalonia UI framework

---
+++ DijkstraVisualization/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DijkstraVisualization.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ DijkstraVisualization/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DijkstraVisualization.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
