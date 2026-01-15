070c4746205488f2e71695716126fbeb6aa1585b
Jan3kk <jasiu4d@wp.pl>
2026-01-16 00:37:56 +0100

    feat: slider now shows 'Instant' when set to 0s, slider ticker is now 0.1

--- DijkstraVisualization/ViewModels/Converters.cs
+++ DijkstraVisualization/ViewModels/Converters.cs
@@ -44,4 +44,23 @@ namespace DijkstraVisualization.ViewModels
             throw new NotImplementedException();
         }
     }
+
+    public class AnimationSpeedConverter : IValueConverter
+    {
+        public static readonly AnimationSpeedConverter Instance = new();
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is double interval)
+            {
+                return interval < 0.01 ? "Instant" : $"{interval:F1}s";
+            }
+            return "0.0s";
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
